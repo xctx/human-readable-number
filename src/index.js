f4d8f67a2bbbd3a528130bd43129200b3c18e1a4
xct <xcat182@gmail.com>
2021-03-11 17:18:28 +0300

    feat: :bug: feat: job is half-done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
+    
+  const digitOnes = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const digitTens = [,,'twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
+
+  const digits = number.toString().split('').reverse();
+
+  let one = digitOnes[number];
+  if (number < 20){
+    return one;
+  }
+
+  let ten = digitTens[digits[0]]+' '+digitOnes[digits[1]];;
+  if ((number < 100) || (digits[1] == 0)){
+    return ten;
+  }
+  
+  if (number < 1000){
+    return digitOnes[digits[0]]+' hundred '+ ten +' '+one;
+  }
   
 }
